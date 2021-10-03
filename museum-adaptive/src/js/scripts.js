417725006ea29fee10dea2a990007046350a18d7
Anton_Dogadin <dogadin00@gmail.com>
2021-10-03 21:23:00 +0300

    feat: add adaptive 1024px

---
+++ museum-adaptive/src/js/scripts.js
@@ -0,0 +1,40 @@
+const play = document.querySelector('.play');
+const pause = document.querySelector('.pause');
+
+let video = document.querySelector('.video_one')
+
+function playf() {
+  video.play();
+}
+function pausef() {
+  video.pause();
+}
+
+play.onclick = playf;
+pause.onclick = pausef;
+
+// const progress = document.querySelectorAll('.progress_style');
+// progress.addEventListener('input', function () {
+//   const value = this.value;
+//   this.style.background = `linear-gradient(to right, 
+//     #710707 0%, #710707 ${value}%, #fff ${value}%, #fff 100%)`
+// })
+
+[...document.querySelectorAll('.progress_style')].forEach(function (item) {
+  item.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, 
+      #710707 0%, #710707 ${value}%, #fff ${value}%, #fff 100%)`
+  })
+})
+
+if (document.querySelectorAll(".hamb").length > 0) {
+  document.querySelector(".hamb").onclick = toggleMobileMenu;
+}
+function toggleMobileMenu() {
+  document.querySelector("html").classList.toggle("menu_open");
+}
+
+
+
+
