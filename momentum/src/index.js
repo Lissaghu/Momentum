e5469d348407a6885bc41e0b60f1cfc063029d94
Anton_Dogadin <dogadin00@gmail.com>
2021-10-22 12:48:20 +0300

    feat: add module quote and author

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,3 +1,5 @@
+// import './js/data.json'
+import './js/scripts.js'
 
 
 
--- momentum/src/js/scripts.js
+++ momentum/src/js/scripts.js
@@ -11,6 +11,10 @@ const weatherDescription = document.querySelector('.weather-description')
 const inputCity = document.querySelector('.city')
 const wind = document.querySelector('.wind')
 const humidity = document.querySelector('.humidity')
+const weatherError = document.querySelector('.weather-error')
+const quote = document.querySelector('.quote')
+const author = document.querySelector('.author')
+const changeQuote = document.querySelector('.change-quote')
 
 
 function showTime() {
@@ -60,9 +64,16 @@ function showGreeting() {
   greet.textContent = greetingText
 }
 
+function localStorageCity() {
+  if (localStorage.getItem('city') === null) {
+    localStorage.setItem('city', 'Minsk')
+  }
+  inputCity.value = localStorage.getItem('city')
+}
+localStorageCity()
+
 function setLocalStorage() {
   localStorage.setItem('name', greetingText.value)
-  localStorage.setItem('city', inputCity.value)
 }
 window.addEventListener('beforeunload', setLocalStorage)
 
@@ -70,8 +81,6 @@ function getLocalStorage() {
   if (localStorage.getItem('name')) {
     greetingText.value = localStorage.getItem('name')
   }
-  inputCity.value = localStorage.getItem('city')
-
 }
 window.addEventListener('load', getLocalStorage)
 
@@ -91,7 +100,6 @@ function setBg() {
   let num = bgNum < 10 ? ('0' + bgNum) : bgNum
   const img = new Image();
   img.src = `https://raw.githubusercontent.com/Lissaghu/stage1-tasks/assets/images/${timeOfDay}/${num}.jpg`
-  console.log(img.src)
   img.addEventListener('load', () => {
     body.style.backgroundImage = `url(${img.src})`
   })
@@ -99,10 +107,10 @@ function setBg() {
 setBg()
 
 function getSlideNext() {
-  if (randomNum <= 20) {
+  if (randomNum < 20) {
     randomNum++
-  } else if (randomNum == 21) {
-    randomNum == 1
+  } else if (randomNum == 20) {
+    randomNum = 1
   }
   setBg()
 }
@@ -112,32 +120,57 @@ slideNext.addEventListener('click', getSlideNext)
 function getSlidePrev() {
   if (randomNum > 1) {
     randomNum--
-  } else if (randomNum == 0) {
-    randomNum == 20
+  } else if (randomNum == 1) {
+    randomNum = 20
   }
   setBg()
 }
 
 slidePrev.addEventListener('click', getSlidePrev)
 
-async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=en&appid=3a8ea49517022bed16bfd3674732aec5&units=metric`
-  const res = await fetch(url)
+// async function getWeather() {
+//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&lang=en&appid=3a8ea49517022bed16bfd3674732aec5&units=metric`
+//   const res = await fetch(url)
+//   console.log(res.status)
+//   const data = await res.json()
+
+//   if (res.status == 200) {
+//     weatherIcon.className = 'weather-icon owf';
+//     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+//     weatherTemperature.textContent = `${Math.round(data.main.temp)}°C`
+//     weatherDescription.textContent = data.weather[0].description
+//     wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`
+//     humidity.textContent = `Humidity: ${data.main.humidity}%`
+//     localStorage.setItem('city', inputCity.value)
+//   } else {
+//     weatherError.textContent = `Error! ${data.message} for ${inputCity.value}`
+//     weatherIcon.style.display = 'none'
+//     weatherTemperature.style.display = 'none'
+//     weatherDescription.style.display = 'none'
+//     wind.style.display = 'none'
+//     humidity.style.display = 'none'
+//   }
+
+// }
+
+// document.addEventListener('DOMContentLoaded', getWeather);
+// inputCity.addEventListener('change', getWeather)
+
+async function getQuotes() {
+  const quotes = 'data.json'
+  const res = await fetch(quotes)
   const data = await res.json()
 
-  weatherIcon.classList.add(`owf-${data.weather[0].id}`)
-  weatherTemperature.textContent = `${Math.round(data.main.temp)}°C`
-  weatherDescription.textContent = data.weather[0].description
-  wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`
-  humidity.textContent = `Humidity: ${data.main.humidity}%`
+  let quoteText = data[Math.floor(Math.random() * 10)].text
+  quote.textContent = quoteText
 
-  localStorage.setItem('temp', weatherTemperature.textContent)
-  if (localStorage.getItem('temp')) {
-    weatherTemperature.textContent = localStorage.getItem('temp')
+  for (let elem of data) {
+    if (quoteText == elem.text) {
+      author.textContent = elem.author
+    }
   }
-
-
 }
-getWeather()
+getQuotes()
 
-inputCity.addEventListener('change', getWeather)
+changeQuote.addEventListener('click', getQuotes)
+
